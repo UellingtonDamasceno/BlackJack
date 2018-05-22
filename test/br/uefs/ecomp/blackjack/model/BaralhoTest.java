8654e2ff3ba26d604143d45c8853e46a78686026
Anésio Sousa <anesios98@gmail.com>
2018-05-22 20:15:54 -0300

    Inserção de novos testes

--- test/br/uefs/ecomp/blackjack/model/BaralhoTest.java
+++ test/br/uefs/ecomp/blackjack/model/BaralhoTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.uefs.ecomp.blackjack.model;
 
 import org.junit.Before;
@@ -11,15 +6,32 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Uellington Damasceno
+ * @author Uellington Damasceno e Anésio Sousa
  */
 public class BaralhoTest {
-    
-    public BaralhoTest() {
-    }
+    private Baralho b;
     
     @Before
     public void setUp() {
+        b = new Baralho(1);
     }
     
+    @Test
+    public void testesBasicos(){
+        Carta[] aux = b.getCartas();
+        
+        assertTrue(aux.length == 52);  
+        assertEquals(aux[0].getFace(), "A");
+        assertEquals(aux[0].getNaipe(), "♦");
+        
+        assertEquals(aux[13].getFace(), "A");
+        assertEquals(aux[13].getNaipe(), "♥");
+        
+        assertEquals(aux[26].getFace(), "A");
+        assertEquals(aux[26].getNaipe(), "♣");
+        
+        assertEquals(aux[39].getFace(), "A");
+        assertEquals(aux[39].getNaipe(), "♠");
+        
+    }
 }
--- test/br/uefs/ecomp/blackjack/model/CartaTest.java
+++ test/br/uefs/ecomp/blackjack/model/CartaTest.java
@@ -38,6 +38,5 @@ public class CartaTest{
         
         assertSame(10, helper);
         
-        
     }
 }
