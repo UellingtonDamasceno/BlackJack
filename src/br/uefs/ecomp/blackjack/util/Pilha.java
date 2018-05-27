8d52ccf611250261ddc8f1a4707618ef030602f2
Anésio Sousa <anesios98@gmail.com>
2018-05-27 17:02:52 -0300

    Mais javadoc

--- src/br/uefs/ecomp/blackjack/util/Pilha.java
+++ src/br/uefs/ecomp/blackjack/util/Pilha.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.uefs.ecomp.blackjack.util;
 
 /**
- * @author Uellington Damasceno
+ * Classe responsável criar objetos que irão conter baralhos usados em partidas.
+ * 
+ * @author Uellington Damasceno e Anésio Sousa
  */
 public class Pilha implements IStack{
     private Celula top;
